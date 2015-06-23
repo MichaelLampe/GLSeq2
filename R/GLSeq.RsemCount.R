d8c692ec04aab683ab0e33ccd6a771813e3dceb1
MichaelLampe <Michaelthelampe@gmail.com>
2015-06-23 14:37:57 -0500

    Counting Protocol Cleanup
    Cleaned a few things up related to logging and added validation to RSEM.
    It will not print to the log file if the SAM file was valid it used was
    valid or not.

--- R/GLSeq.RsemCount.R
+++ R/GLSeq.RsemCount.R
@@ -11,6 +11,12 @@ setwd(dest.dir)
 if (is.null(this.resName)) this.resName <- text.add
 #
 #
+# Prints out a check to the log file if the counting file is correct.
+#
+header.message <- paste("echo \"Checking SAM files for RSEM compatability. The file is: ",countable.sam,"\"",sep="")
+header.file <- paste(header.message,">> ",log.file)
+check.file <- paste("rsem-sam-validator",countable.sam,">>",log.file)
+#
 refName <- paste(this.resName,"RSEM","index",sep=".")
 prepareReference <- paste("rsem-prepare-reference",refFASTAname,refName)
 #
@@ -22,6 +28,6 @@ if (!paired.end) rsemOptions <- paste(rsemOptions,"--sam")
 if (!is.na(countable.sam)){
   calculateExpression <- paste("rsem-calculate-expression",rsemOptions,countable.sam,refName,countFile)
   # Movement
-  if (count.comm != "") count.comm <- paste(count.comm, "&&", prepareReference, "&&",calculateExpression)
-  if (count.comm == "") count.comm <- paste(prepareReference, "&&", calculateExpression)
+  if (count.comm != "") count.comm <- paste(count.comm,"&&",check.file,"&&", prepareReference, "&&",calculateExpression)
+  if (count.comm == "") count.comm <- paste(header.file,"&&",check.file,"&&",prepareReference, "&&", calculateExpression)
 }
