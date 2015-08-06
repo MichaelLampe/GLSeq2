555c0ffbb64fc1aefe42f89fac5c7e0589d1bf8f
MichaelLampe <Michaelthelampe@gmail.com>
2015-08-06 12:48:10 -0500

    Expanded Condor Functionality
    Now better groups "Fast" jobs.  Soon to come: Resource allocation by job
    type.

--- PythonWrapper/CommandStack.py
+++ PythonWrapper/CommandStack.py
@@ -13,6 +13,7 @@ class Stack:
 
         self.group_keywords = [
             # The space is important for keeping them as just the linux commands
+            'date ',
             'mkdir ',
             'cd ',
             'mv ',
@@ -27,25 +28,47 @@ class Stack:
         # Connects steps that have a key word
         # The last step is always either the end of this parallel command
         # Or a larger step that is not a keyword in hopes of creating "larger" chunked steps.
-        for steps in self.command_list:
-            for step in steps:
-                for x in range (len(step) - 1 ,0,-1):
-                    for word in self.group_keywords:
-                        if (word in step[x - 1]):
-                            if (x != 0):
-                                step[x - 1] = step[x-1] + " && " + step[x]
-                                step.pop(x)
-                                # Exit after you find one because that's all good
-                                break
+        #
+        # The format of the command_list is as follows:
+        # [All individual commands received][Commands executed in parallel][The linked commands within each of those]
+        # Individual Commands
+        for z in range(len(self.command_list) - 1,-1,-1):
+            # Parallel command
+            self.concatenate_commands(self.command_list,z)
+            for y in range (len(self.command_list[z]) -1,-1,-1):
+                # Commands linked together
+                for x in range (len(self.command_list[z][y]) - 1 ,-1,-1):
+                    self.concatenate_linked_commands(self.command_list[z][y],x)
         for parent in range (0, len(self.command_list)):
             for step in self.command_list[parent]:
                 command = Command(step,parent)
                 self.command_stack.append(command)
-
         for command in self.command_stack:
             command.create_bash_files(run_name)
             self.create_submit_file(run_name)
             command.create_dag_jobs(run_name)
+
+    def concatenate_commands(self,command,current_index):
+        for word in self.group_keywords:
+            if (word in command[current_index][0][0]):
+                if (current_index != 0):
+                    # Adds the previous command to the one ahead of it if it is in the list
+                    command[current_index -1][0] += command[current_index][0]
+                    # Then removes the command just duplicated from its original location
+                    command[current_index].pop(0)
+                    # Checks if the location is now empty and removes it if it is
+                    if(len(command[current_index]) < 1):
+                        command.pop(current_index)
+
+    def concatenate_linked_commands(self,command,current_index):
+        for word in self.group_keywords:
+            if (word in command[current_index - 1]):
+                if (current_index != 0):
+                    # This links members of the same step/not commands printed individually
+                    command[current_index - 1] = command[current_index-1] + " && " + command[current_index]
+                    command.pop(current_index)
+                    # Exit after you find one because that's all good
+                    break
 
     def create_dag_workflow(self,run_name):
         mydag = Dagfile()
@@ -72,10 +95,12 @@ class Stack:
         with open(str(submit_file_name),'w') as submit_file:
             submit_file.write("universe = vanilla\n")
             submit_file.write("executable = $(execute)\n")
+            # Only run on linux
+            submit_file.write("requirements = OpSys == \"LINUX\"\n")
             #submit_file.write("arguments = $(args)\n")
-            submit_file.write("log = job.log\n")
-            submit_file.write("out = job.out\n")
-            submit_file.write("err = job.err\n")
+            submit_file.write("log = $(log)\n")
+            submit_file.write("out = $(output)\n")
+            submit_file.write("err = $(error)\n")
             submit_file.write("request_memory = $(mem)\n")
             submit_file.write("request_cpus = $(cpus)\n")
             submit_file.write("queue\n")
