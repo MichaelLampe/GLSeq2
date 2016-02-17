faf4ade23c0551af55fce7339c9ab892f6c92092
MichaelLampe <Michaelthelampe@gmail.com>
2016-02-17 13:33:01 -0600

    Fixed Attribute File Problems
    JSON is again valid after bug report.

--- User Interface/src/application/AttributeActionsCommandLine.java
+++ User Interface/src/application/AttributeActionsCommandLine.java
@@ -5,20 +5,19 @@ import java.util.ArrayList;
 public final class AttributeActionsCommandLine extends AttributeFileWriter {
 
 	public void setAttributes(String[] inputArgs) {
-		ArrayList<AttributeFactorySingleton.Attribute> attributeArray = AttributeFactorySingleton
-				.getInstance().getAllAttributes();
+		ArrayList<AttributeFactorySingleton.Attribute> attributeArray = AttributeFactorySingleton.getInstance()
+				.getAllAttributes();
 		for (String param : inputArgs) {
-			
+
 			/*
 			 * Left side is the key, right side is the value.
 			 */
-			String[] parts = param.split("=");		
-			
+			String[] parts = param.split("=");
+
 			for (AttributeFactorySingleton.Attribute att : attributeArray) {
 				if (att.getUiName().equals(parts[0])) {
 					try {
-						AttributeFactorySingleton.getInstance()
-								.setAttributeValue(att.getName(), parts[1]);
+						AttributeFactorySingleton.getInstance().setAttributeValue(att.getName(), parts[1]);
 					} catch (NoSuchKeyInAttributeFactoryException e1) {
 						/*
 						 * Did not find attribute key.
@@ -31,10 +30,10 @@ public final class AttributeActionsCommandLine extends AttributeFileWriter {
 
 	public void returnJson() {
 		System.out.println("{\"attributes\":[");
-		ArrayList<AttributeFactorySingleton.Attribute> attributeArray = AttributeFactorySingleton
-				.getInstance().getAllAttributes();
+		ArrayList<AttributeFactorySingleton.Attribute> attributeArray = AttributeFactorySingleton.getInstance()
+				.getAllAttributes();
 		for (int i = attributeArray.size() - 1; i >= 0; i--) {
-			
+
 			/*
 			 * We want to give them all the categories that are not in the
 			 * script.
@@ -43,12 +42,11 @@ public final class AttributeActionsCommandLine extends AttributeFileWriter {
 				attributeArray.remove(i);
 			}
 		}
-		
+
 		for (int i = 0; i < attributeArray.size(); i++) {
-			
+
 			/*
-			 * They have SSH'd in, so they can read the system out
-			 * stream.
+			 * They have SSH'd in, so they can read the system out stream.
 			 */
 			try {
 				System.out.print(formatJson(attributeArray.get(i).getUiName()));
@@ -68,8 +66,7 @@ public final class AttributeActionsCommandLine extends AttributeFileWriter {
 	/*
 	 * JSON formatted String to return to GLOW
 	 */
-	private String formatJson(String uiName)
-			throws NoSuchKeyInAttributeFactoryException {
+	private String formatJson(String uiName) throws NoSuchKeyInAttributeFactoryException {
 		String jsonKey = "{\"key\":";
 		AttributeFactorySingleton.Attribute current;
 		current = AttributeFactorySingleton.getInstance().getAttribute(uiName);
@@ -78,11 +75,31 @@ public final class AttributeActionsCommandLine extends AttributeFileWriter {
 			jsonKey += ", \"category\":\"" + current.getCategory() + "\"";
 		}
 		if (current.getOptions() != null) {
-			jsonKey += ", \"options\":[" + current.getOptionsPresplit() + "]";
+			String s = current.getOptionsPresplit().replace("\\", "");
+
+			if (!s.equals("")) {
+				String[] splitValues = s.split(",");
+				s = "";
+				for (int i = 0; i < splitValues.length; i++) {
+					splitValues[i] = splitValues[i].trim();
+					splitValues[i] = splitValues[i].replace("\n", "");
+					if (!(splitValues[i].charAt(0) == '"')) {
+						splitValues[i] = '"' + splitValues[i];
+					}
+					if (!(splitValues[i].charAt(splitValues[i].length() - 1) == '"')) {
+						splitValues[i] = splitValues[i] + '"';
+					}
+					if (i != 0) {
+						s += ",";
+					}
+					s += splitValues[i];
+				}
+			}
+
+			jsonKey += ", \"options\":[" + s + "]";
 		}
 		if (current.getDefault() != null) {
-			jsonKey += ", \"default\":\""
-					+ defaultNullIsEmpty(current.getDefault()) + "\"";
+			jsonKey += ", \"default\":\"" + defaultNullIsEmpty(current.getDefault()) + "\"";
 		}
 		if (current.getToolTip() != null) {
 			jsonKey += ", \"description\":\"" + current.getToolTip() + "\"";
