55c0820b9e80adac88e8c5c612c9d9c9d8b86686
MichaelLampe <Michaelthelampe@gmail.com>
2016-02-26 15:08:34 -0600

    Updated Conversion Scripts
    The conversion scripts have been updated to be usable outside of the
    command line.  Cleaned up the comments as well.

--- PythonWrapper/GtfToPtt.py
+++ PythonWrapper/GtfToPtt.py
@@ -1,21 +1,24 @@
 __original_author__ = 'Michael Lampe'
-# If updated by another person, please change the values below
 __author__ = 'Michael Lampe'
-__version__ = '1.0'
-__last_update__ = "October 2015"
+__version__ = '1.0.1'
+__last_update__ = "Febuary 2016"
 
 import os
 import re
 import sys
 
-# Normal command default values
-file_name = ""
-directory_name = ""
+"""
+Precompile regexes
+"""
+GENE_ID_REGEX = re.compile("(?<=gene_id \").*(?=\"; transcript_id)", re.IGNORECASE)
 
-# Advanced command default values
-gtf_column_count = 9
+"""
+File extension
+"""
+PTT_FILE_EXTENSION = ".ptt"
+
 # User interaction stuff
-def print_help():
+def _print_help():
     print "HELP FOR GTF/GFF TO PTT FILE CONVERTER:\n"
     print "Originally written by " + __original_author__
     print "Last Updated by " + __author__ + ", " + __last_update__ + "\n"
@@ -33,141 +36,175 @@ def print_help():
     print "OPTION_COMMAND = NUMBER_OF_INPUT_FILE_COLUMNS"
     print "DEFAULT_VALUE = 9\n"
 
-# Check args length
-if len(sys.argv) < 3:
-    print_help()
 
-for i , command in enumerate(sys.argv):
-    # Make sure it is a string
-    command = str(command)
-    if i == 0:
-        pass
-    elif i == 1:
-        # Get input GTF file
-        file_name = command
-    elif i == 2:
-        # Directory to write files and folders to
-        directory_name = command
+def convert_gtf_to_ptt(gtf_file, reference_genome_destination, gtf_column_count=9):
+    """
+    Converts a gtf file into a ptt file based on the expected formatting
 
-    # Advanced commands
-    elif i > 2:
-        try:
-            split_command = command.split("=")
-            option_name, opt_value = split_command[0], split_command[1]
+    :param gtf_file: The input GTF file to be converted
+    :param reference_genome_destination: The destination of our converted file.
+    :param gtf_column_count: The number of columns in the GTF file that is to be converted.
+    :return:
+    """
 
-            command_executed = False
-            if option_name is "NUMBER_OF_INPUT_FILE_COLUMNS":
-                gtf_column_count = int(opt_value)
-                command_executed = True
-            # If user supplied a dud advanced command
-            if not command_executed:
-                print "ERROR: Advanced option command: " + option_name + " could not be matched with any " \
-                    "advanced options.  Please check input or type GtfToPtt.py --help for additional help."
-                exit(4)
-        # If values improperly linked
-        except IndexError:
-            print "ERROR: Advanced command was incorrectly used. Please input advanced commands in the format:" \
-                    "\n COMMAND_NAME=COMMAND_VALUE\n and only pass them after the file name and directory name."
-            exit(3)
+    """
+    Append trailing slash
+    """
+    if not reference_genome_destination.endswith("/"):
+        directory_name = reference_genome_destination + "/"
 
-# Check if proper file type
-if not file_name.endswith("gtf") and not file_name.endswith("gff"):
-    print "ERROR: Input file to be converted should have either gtf or gff file ending."
-    exit(2)
+    """
+    Format species name based on GTF/GFF file name.
+    Remove gtf file ending and absolute file path stuff
+    """
+    species = re.search("[0-9a-zA-Z_.]*$", gtf_file, re.IGNORECASE).group(0)
+    species = species.split(".")[0]
 
-# Append trailing slash
-if not directory_name.endswith("/"):
-    directory_name = directory_name + "/"
+    """
+    PTT File Format
+    Location	Strand	Length	PID	Gene	Synonym	Code	COG	Product
 
-# Precompile common regex
-gene_id = re.compile("(?<=gene_id \").*(?=\"; transcript_id)", re.IGNORECASE)
+    Dictionary containing the name of each sequence found in the single gtf/gff file and the number of proteins that
+    Have been added to these files.
+    """
+    files_data = dict()
+    with open (gtf_file) as file:
+        for line in file:
+            div_in = line.split("\t")
+            if not (len(div_in) < gtf_column_count):
 
-# Format species name based on GTF/GFF file name.
-# Remove gtf file ending and absolute file path stuff
-species = re.search("[0-9a-zA-Z_.]*$", file_name, re.IGNORECASE).group(0)
-species = species.split(".")[0]
-
-"""
-PTT File Format
-Location	Strand	Length	PID	Gene	Synonym	Code	COG	Product
-
-Dictionary containing the name of each sequence found in the single gtf/gff file and the number of proteins that
-Have been added to these files.
-"""
-files_data = dict()
-with open (file_name) as file:
-    for line in file:
-        div_in = line.split("\t")
-        if not (len(div_in) < gtf_column_count):
-            """
-            The sequence name is the first item in a gtf/gff file
-            If we don't have a matching key, we'll want to do some things to establish the given
-            Sequence within our structure
-            """
-            if div_in[0] not in files_data:
-                # Create folder
-                folder_name = directory_name + div_in[0] + "/"
-                new_ptt_file = folder_name + div_in[0]
-                # Makes directory if not available
-                if not os.path.exists(os.path.dirname(folder_name)):
-                    os.makedirs(os.path.dirname(folder_name))
-                # Create file with header
-                # Species, which chromosome
-                h_line_1 = species + ", " + div_in[0]
-                # Number of proteins.  We don't know this when writing this so we just give it a dummy value
-                h_line_2 = str("<?> proteins")
-                # Description of column meanings
-                h_line_3 = "Location\tStrand\tLength\tPID\tGene\tSynonym\tCode\tCOG\tProduct"
                 """
-                Add to dictionary so this doesn't occur again
-                At index 0 we have the number of proteins (Which the first one will be 1)
-                At index 1 we have a list of all the lines.  This saves us from rewriting the entire file when we want
-                to finally write to a file.
+                The sequence name is the first item in a gtf/gff file
+                If we don't have a matching key, we'll want to do some things to establish the given
+                Sequence within our structure
                 """
-                files_data[div_in[0]] = [1, list()]
-                # If we add these now we won't need to do an insert when we want to input the number of proteins
-                files_data[div_in[0]][1].append(h_line_1)
-                files_data[div_in[0]][1].append(h_line_2)
-                files_data[div_in[0]][1].append(h_line_3)
-            # File Name
-            ptt_file = directory_name + div_in[0] + "/" + div_in[0]
-            # Location
-            location = div_in[3] + ".." + div_in[4]
-            # Strand
-            strand = div_in[6]
-            # Length of protein
-            length = str(int(div_in[4]) - int(div_in[3]) - 2)
-            # PID Gene
-            pid = "-"
-            # Gene
-            gene = re.search(gene_id, line).group(0)
-            # Synonym
-            syn = str(species) + "_" + str(files_data[div_in[0]][0])
-            # Code
-            code = "-"
-            # COG
-            cog = "-"
-            # Product
-            product = "-"
-            # Increase protein number counts
-            files_data[div_in[0]][0] = files_data[div_in[0]][0] + 1
-            ptt_file_line = location + "\t" + \
-                            strand + "\t" + \
-                            length + "\t" + \
-                            pid + "\t" + \
-                            gene + "\t" + \
-                            syn + "\t" + \
-                            code + "\t" + \
-                            cog + "\t" + \
-                            product
+                if div_in[0] not in files_data:
 
-            files_data[div_in[0]][1].append(ptt_file_line)
+                    """
+                    Create folder
+                    """
+                    folder_name = directory_name + div_in[0] + "/"
+                    new_ptt_file = folder_name + div_in[0]
 
-for key in files_data:
-    # Fix the number of the proteins in the header of each file
-    # Then create the files.
-    files_data[key][1][1] = str(files_data[key][0] - 1) + " proteins"
-    file_name = directory_name + key + "/" + key + ".ptt"
-    with open(file_name, "w") as new_ptt_file:
-        for line in files_data[key][1]:
-            new_ptt_file.write(line + "\n")
+                    """
+                    Makes directory if not available
+                    """
+                    if not os.path.exists(os.path.dirname(folder_name)):
+                        os.makedirs(os.path.dirname(folder_name))
+
+                    """
+                    Create file with header
+                    Species, which chromosome
+                    """
+                    h_line_1 = species + ", " + div_in[0]
+
+                    """
+                    Number of proteins.  We don't know this when writing this so we just give it a dummy value
+                    """
+                    h_line_2 = str("<?> proteins")
+
+                    """
+                    Description of column meanings
+                    """
+                    h_line_3 = "Location\tStrand\tLength\tPID\tGene\tSynonym\tCode\tCOG\tProduct"
+
+                    """
+                    Add to dictionary so this doesn't occur again
+                    At index 0 we have the number of proteins (Which the first one will be 1)
+                    At index 1 we have a list of all the lines.  This saves us from rewriting the entire file when we want
+                    to finally write to a file.
+                    """
+                    files_data[div_in[0]] = [1, list()]
+
+                    """
+                    If we add these now we won't need to do an insert when we want to input the number of proteins
+                    """
+                    files_data[div_in[0]][1].append(h_line_1)
+                    files_data[div_in[0]][1].append(h_line_2)
+                    files_data[div_in[0]][1].append(h_line_3)
+
+                ptt_file = directory_name + div_in[0] + "/" + div_in[0]         # File Name
+                location = div_in[3] + ".." + div_in[4]                         # Location
+                strand = div_in[6]                                              # Strand
+                length = str(int(div_in[4]) - int(div_in[3]) - 2)               # Length of protein
+                pid = "-"                                                       # PID Gene
+                gene = re.search(GENE_ID_REGEX, line).group(0)                  # Gene
+                syn = str(species) + "_" + str(files_data[div_in[0]][0])        # Synonym
+                code = "-"                                                      # Code
+                cog = "-"                                                       # COG
+                product = "-"                                                   # Product
+                files_data[div_in[0]][0] = files_data[div_in[0]][0] + 1         # Increase protein number counts
+
+                ptt_file_line = location + "\t" + \
+                                strand + "\t" + \
+                                length + "\t" + \
+                                pid + "\t" + \
+                                gene + "\t" + \
+                                syn + "\t" + \
+                                code + "\t" + \
+                                cog + "\t" + \
+                                product
+
+                files_data[div_in[0]][1].append(ptt_file_line)
+
+    for key in files_data:
+        """
+        Fix the number of the proteins in the header of each file
+        Then create the files.
+        """
+        files_data[key][1][1] = str(files_data[key][0] - 1) + " proteins"
+        file_name = directory_name + key + "/" + key + PTT_FILE_EXTENSION
+
+        with open(file_name, "w") as new_ptt_file:
+            for line in files_data[key][1]:
+                new_ptt_file.write(line + "\n")
+
+
+if __name__ == "__main__":
+
+    # Check args length
+    if len(sys.argv) < 3:
+        _print_help()
+
+    gtf_file = ""
+    directory_name = ""
+    gtf_column_count = 9
+
+    for i , command in enumerate(sys.argv):
+        command = str(command)                  # Make sure it is a string
+        if i == 0:
+            pass                                # This is the script name, just ignore it
+        elif i == 1:
+            gtf_file = command                 # Get input GTF file
+        elif i == 2:
+            directory_name = command            # Directory to write files and folders to
+        elif i > 2:                             # Advanced commands
+            try:
+                split_command = command.split("=")
+                option_name, opt_value = split_command[0], split_command[1]
+
+                command_executed = False
+                if option_name is "NUMBER_OF_INPUT_FILE_COLUMNS":
+                    gtf_column_count = int(opt_value)
+                    command_executed = True
+
+                """
+                If user supplied a dud advanced command
+                """
+                if not command_executed:
+                    print "ERROR: Advanced option command: " + option_name + " could not be matched with any " \
+                        "advanced options.  Please check input or type GtfToPtt.py --help for additional help."
+                    exit(4)
+            # If values improperly linked
+            except IndexError:
+                print "ERROR: Advanced command was incorrectly used. Please input advanced commands in the format:" \
+                        "\n COMMAND_NAME=COMMAND_VALUE\n and only pass them after the file name and directory name."
+                exit(3)
+
+    # Check if proper file type
+    if not gtf_file.endswith("gtf") and not gtf_file.endswith("gff"):
+        print "ERROR: Input file to be converted should have either gtf or gff file ending."
+        exit(2)
+
+    convert_gtf_to_ptt(gtf_file, directory_name, gtf_column_count)
+    exit(0)
--- PythonWrapper/checkRhEnvironment.py
+++ PythonWrapper/checkRhEnvironment.py
@@ -1,31 +1,58 @@
 __author__ = 'Michael Lampe'
 
-import sys
 import shutil
 import os
 
 
-directoryToCheck = sys.argv[1]
-if not directoryToCheck.endswith("/"):
-    directoryToCheck += "/"
-fna_ext = ".fna"
-ptt_ext = ".ptt"
+def rh_environment_check(check_directory):
+    """
+    Rockhopper aligner requires files where both PTT and FNA files are expected.
+    This script is utilized to check individual directories and ask if they contain both of these file types.
+    If both are not present, they are removed from the file system.
 
-# Looks into the directory that is supposed to be checked
-for directory in os.listdir(directoryToCheck):
-    directory = directoryToCheck + directory
-    fna, ptt = False, False
+    :param check_directory:
+    :return:
+    """
+    FNA_EXT = ".fna"
+    PTT_EXT = ".ptt"
 
-    # Goes through the files in a given directory looking for certain file types.
-    for files in os.listdir(directory):
-        files = directory + files
-        if files.endswith(fna_ext):
-            fna = True
-        elif files.endswith(ptt_ext):
-            ptt = True
+    if not check_directory.endswith("/"):  # Check trailing directory
+        check_directory += "/"
 
-    # Remove the directory file if it doen't contain both an FNA and a PTT file
-    if not fna or not ptt:
-        shutil.rmtree(directory)
+    """
+    Looks into the directory that is supposed to be checked
+    """
+    for directory in os.listdir(check_directory):
+        directory = check_directory + directory
+        has_fna, has_ptt = False, False
 
-exit(0)
+        """
+        Goes through the files in a given directory looking for certain file types.
+        """
+        for files in os.listdir(directory):
+            files = directory + files
+
+            if files.endswith(FNA_EXT):
+                has_fna = True
+            elif files.endswith(PTT_EXT):
+                has_ptt = True
+
+        """
+        Remove the directory file if it doesn't contain both an FNA and a PTT file
+        """
+        if not has_fna or not has_ptt:
+            shutil.rmtree(directory)
+
+
+if __name__ == "__main__":
+    import sys
+
+    """
+    Get the single command line arg
+    """
+    directory_to_check = sys.argv[1]
+    try:
+        rh_environment_check(directory_to_check)
+        exit(0)
+    except:
+        exit(1)
